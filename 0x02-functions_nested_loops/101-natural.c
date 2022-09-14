cc54b9611b127141675f802e2fb6d7f8b08c55b3
perfumegoddess <queenjossey@gmail.com>
2022-09-14 05:28:46 +0100

    looped

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
 
 /**
- * main - finds and prints the sum of the even-valued terms
- * followed by a new line
+ * main - computes and prints the sum of all the multiples
+ * of 3 or 5 below 1024
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	unsigned long int sum3, sum5, sum;
 	int i;
-	unsigned long int j, k, next, sum;
 
-	j = 1;
-	k = 2;
+	sum3 = 0;
+	sum5 = 0;
 	sum = 0;
 
-	for (i = 1; i <= 33; ++i)
+	for (i = 0; i < 1024; ++i)
 	{
-		if (j < 4000000 && (j % 2) == 0)
+		if ((i % 3) == 0)
 		{
-			sum = sum + j;
+			sum3 = sum3 + i;
+		} else if ((i % 5) == 0)
+		{
+			sum5 = sum5 + i;
 		}
-		next = j + k;
-		j = k;
-		k = next;
 	}
-
+	sum = sum3 + sum5;
 	printf("%lu\n", sum);
-
 	return (0);
 }
